c817824f435954fe0a9966ac067f768819163661
jeoninho623 <0623inho@naver.com>
2023-10-10 15:20:13 +0900

    Maven

---
+++ web_study/day01/src/main/java/org/koreait/Member.java
@@ -0,0 +1,11 @@
+package org.koreait;
+import lombok.*;
+
+@Getter @Setter @ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Member {
+    private String userId;
+    private String userPw;
+    private String userNm;
+}
