3d4cb5eef0b913f13ca657dd0cfb26c2535e3313
jeoninho623 <0623inho@naver.com>
2023-10-10 17:51:16 +0900

    TDD

--- web_study/day02/src/test/java/models/member/JoinService.java
+++ web_study/day02/src/test/java/models/member/JoinService.java
@@ -1,6 +1,9 @@
 package models.member;
 
 public class JoinService {
-    public void join() {
-
+    public void join(Member member) {
+        String userId = member.getUserId();
+        if(userId == null || userId.isBlank()) {
+            throw new BadRequestException("아이디를 입력하세요");
+        }
     }
