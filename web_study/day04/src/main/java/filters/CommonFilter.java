80218719723dd772db61b1e911da72f13f0ebfc4
jeoninho623 <0623inho@naver.com>
2023-10-13 15:52:01 +0900

    servlet

---
+++ web_study/day04/src/main/java/filters/CommonFilter.java
@@ -0,0 +1,23 @@
+package filters;
+
+import jakarta.servlet.*;
+
+import java.io.IOException;
+
+public class CommonFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain filterChain) throws IOException, ServletException {
+        /*
+        HttpServletRequest request = (HttpServletRequest)req;
+        String method = request.getMethod().toUpperCase();
+        if (method.equals("POST")) {
+            request.setCharacterEncoding("UTF-8");
+        }
+        */
+
+
+
+        filterChain.doFilter(new CommonRequestWrapper(req), new commonResponseWrapper(resp));
+    }
+}
