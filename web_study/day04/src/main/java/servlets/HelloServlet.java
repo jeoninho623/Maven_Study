27eb6735d2d967aaef566f22855b5b6624ebf949
jeoninho623 <0623inho@naver.com>
2023-10-13 14:50:50 +0900

    servlet

--- web_study/day04/src/main/java/servlets/HelloServlet.java
+++ web_study/day04/src/main/java/servlets/HelloServlet.java
@@ -1,5 +1,6 @@
 package servlets;
 
+import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -10,7 +11,10 @@ import java.io.IOException;
 public class HelloServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("안녕하세요~");
+        System.out.println("do Get!");
+
+        String common1 = req.getServletContext().getInitParameter("common1");
+        System.out.println(common1);
     }
 
     @Override
@@ -19,7 +23,21 @@ public class HelloServlet extends HttpServlet {
     }
 
     @Override
+    public void init(ServletConfig config) throws ServletException {
+        String key1 = config.getInitParameter("key1");
+        String key2 = config.getInitParameter("key2");
+        System.out.printf("key1=%s, key2=%s%n", key1, key2);
+
+        String common1 = config.getInitParameter("common1");
+        String common2 = config.getInitParameter("common2");
+        System.out.printf("common1=%s, common2=%s%n", common1, common2);
+
+    }
+
+    /*@Override
     public void init() throws ServletException {
         System.out.println("init!!!");
-    }
+    }*/
+
+
 }
