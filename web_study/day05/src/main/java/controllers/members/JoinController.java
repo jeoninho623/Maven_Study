8c7e07b36be2fd7461aebbae7066c4d9b7c48bd2
jeoninho623 <0623inho@naver.com>
2023-10-16 17:19:55 +0900

    JSP

--- web_study/day05/src/main/java/controllers/members/JoinController.java
+++ web_study/day05/src/main/java/controllers/members/JoinController.java
@@ -8,14 +8,21 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/member/join")
 public class JoinController extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/templates/join.jsp");
-        rd.forward(req, resp);
+        resp.setContentType("text/html; charset=UTF-8");
+        PrintWriter out = resp.getWriter();
+        out.write("<h1>상단</h1>");
+
+        RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/templates/member/join.jsp");
+        rd.include(req, resp);
+
+        out.write("<h1>하단</h1>");
     }
 
     @Override
